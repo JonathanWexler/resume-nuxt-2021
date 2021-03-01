05904a3f763bac7c4ef3c1e34dc109d471bd4373
Jon Wexler <jwexman@gmail.com>
2021-03-01 03:04:23 -0500

    [master] adds twitch link

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
 
           <p>Listen to my <a
               href="http://therabbithole.libsyn.com/102-digging-into-javascript-w-jonathan-wexler">podcast
-              interview on Node.js</a> with The Rabbit Hole. Watch my <a href="https://www.twitch.tv/videos/808195944">Twitch livestream on Node.js</a>, hosted by Manning Publications.
+              interview on Node.js</a> with The Rabbit Hole. Watch my <a href="https://www.twitch.tv/videos/808195944">Twitch livestream on Node.js</a> and <a href="https://www.twitch.tv/manningpublications/video/926422598">APIs</a> hosted by Manning Publications.
           </p>
           </section>
         </b-col>
